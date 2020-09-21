778e386fdb27dc5debe8efd236dfe43951292f13
BabyBabaBabu <iamkitaaresort@gmail.com>
2020-09-21 23:43:42 +0300

    added an  automatic ssh keys generator

---
+++ Utilities/ssh-autolog.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+remotehost=$2
+user=$1
+
+function usage(){
+	echo -e "$0 - autogenerate ssh keys for passwordless login to a host"
+	echo -e "Usage:\t./$0 [ruser] [rhost]"
+	echo -e "\truser - remote user account"
+	echo -e "\trhost - remote host machine"
+
+}
+
+function autolog(){
+
+	ssh-keygen -t rsa -b 4096 -N "" -f ${remotehost} &> /dev/null
+
+	echo -e "[+]\tKey generated here `pwd`"
+	ls -l | grep ${remotehost}
+	echo -e "[+]\tCopying public key to ${remotehost}"
+	ssh-copy-id -i ${remotehost}.pub ${user}@${remotehost}
+
+}
+
+#if no argument passed; print usage; else generate keys
+if [[ $# -eq 0 || $# -lt 2 ]] ; then
+	usage
+	exit 1
+else
+	autolog
+fi
+
+
+
+
+
