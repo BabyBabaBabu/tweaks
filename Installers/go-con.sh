7d56476e105fd21a832d17219d5f8618905c6ddd
BabyBabaBabu <iamkitaaresort@gmail.com>
2020-12-29 22:25:37 +0300

    automated go project workspace creation based on GOPATH

--- Installers/go-con.sh
+++ Installers/go-con.sh
@@ -3,7 +3,13 @@
 PROJECT=$1
 # echo $PROJECT
 # locate gopath for projects
-DIR=`go env GOPATH | awk -FS":" '{print $1}'`
+DIR=`go env GOPATH | awk -F":" '{print $2}'`
+# echo $DIR
 # create new workspace for project
 mkdir -p $DIR/src/github.com/BabyBabaBabu/$PROJECT
-echo -e "[+] Done, Happy Hacking!!"
+
+if [ $? -eq 0 ];then
+    echo -e "[+] Done, Happy Hacking!! :-)"
+else
+    echo -e "[!] Set up failed; something went wrong :-("
+fi
