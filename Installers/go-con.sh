69abb79f6c858c357fb3a4edfd6cf86b848719a2
BabyBabaBabu <iamkitaaresort@gmail.com>
2020-12-29 22:30:42 +0300

    pull github user name from global git config

--- Installers/go-con.sh
+++ Installers/go-con.sh
@@ -4,9 +4,10 @@ PROJECT=$1
 # echo $PROJECT
 # locate gopath for projects
 DIR=`go env GOPATH | awk -F":" '{print $2}'`
+GITPROFILE=`git config --global --list | grep user.name | awk -F= '{print $2}'`
 # echo $DIR
 # create new workspace for project
-mkdir -p $DIR/src/github.com/BabyBabaBabu/$PROJECT
+mkdir -p $DIR/src/github.com/$GITPROFILE/$PROJECT
 
 if [ $? -eq 0 ];then
     echo -e "[+] Done, Happy Hacking!! :-)"
